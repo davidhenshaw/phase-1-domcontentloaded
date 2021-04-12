0c074eb2291b7816d9cb7e84bb51246eafadd524
davidhenshaw <davidhenshaw2014@gmail.com>
2021-04-12 14:11:15 -0500

    commit tree 6c976c289d4f098d7f328c3d29ed3add46199307

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let p = document.querySelector("#text");
+//console.log(p);
+
+document.addEventListener('DOMContentLoaded', function(){
+    p.textContent = "This is really cool!";
+});
